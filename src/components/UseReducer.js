635bb4fffb107aba64c4c70f14c08a2d1c516687
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2021-08-19 15:30:05 +0500

    useReducer

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,28 +1,31 @@
-import React, { useReducer } from "react"
 
-const initialState = 0
-const reducer = (currentState, action) => {
+import React,{useReducer} from "react";
+
+const initialState = 0;
+const reducer = (currentState , action) =>{
     switch (action){
         case "increment":
-            return currentState + 1
+            return currentState +1
         case "decrement":
-            return currentState - 1
+            return currentState -1
         case "reset":
             return initialState
-        default:
+        case "defalt":
             return currentState
+
     }
 }
-const UseReducer = () => {
-    const [count, dispatch] = useReducer(reducer, initialState)
-    return (
-        <div>
-            <div>count - {count}</div>
-            <button onClick={() => dispatch("increment")}>Increment</button>
-            <button onClick={() => dispatch("decrement")}>Decrement</button>
-            <button onClick={() => dispatch("reset")}>Reset</button>
-        </div>
-    )
-}
 
-export default UseReducer
+const UseReducer = () =>{
+    const [count ,dispatch] = useReducer(reducer,initialState)
+    return(
+        <div>
+        <div> count - {count}</div>
+
+        <button onClick={() => dispatch("increment")}>incremnt</button>
+        <button onClick={() => dispatch("decrement")}>decrement</button>
+        <button onClick={() => dispatch("reset")}>reset</button>
+
+    </div>)
+}
+export default UseReducer;
